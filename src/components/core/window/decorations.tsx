b83d365ca16853e3c5ea5069eb911c103cb7ff7f
Mufaro <mufaro.priv@gmail.com>
2024-03-27 00:28:01 +0100

    fix: Holding on window title doesn't make the window draggable

--- src/components/core/window/decorations.tsx
+++ src/components/core/window/decorations.tsx
@@ -7,14 +7,16 @@ export default function WindowDecorations() {
       data-tauri-drag-region
       className="h-[30px] bg-sidebar-bg-color select-none justify-between flex items-center top-0 right-0 left-0"
     >
-      <div className="w-11 flex justify-center">
+      <div className="w-12 flex justify-center">
         <img src="/icons/icon.png" className="w-6 h-6 mt-2" />
       </div>
-      <span className="text-sm text-white font-semibold">Genshin Loader</span>
+      <p data-tauri-drag-region className="text-sm text-white font-semibold">
+        Genshin Loader
+      </p>
       <div className="flex justify-end">
         <button
           onClick={() => appWindow.close()}
-          className="inline-flex justify-center items-center w-[30px] h-[30px]"
+          className="inline-flex justify-center rounded-none items-center w-[30px] h-[30px] hover:bg-red-500 hover:bg-opacity-50 transition-colors duration-150 ease-in-out"
         >
           <X size={20} />
         </button>
