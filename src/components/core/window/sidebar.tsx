ffd5da2d3d191caa1197528078f7a5940f5e2ba3
Mufaro <mufaro.priv@gmail.com>
2024-03-29 18:22:20 +0100

    fix: Allow sidebar to be used when installing game

--- src/components/core/window/sidebar.tsx
+++ src/components/core/window/sidebar.tsx
@@ -1,17 +1,9 @@
 import { Box, Home } from "lucide-react";
 import { Tab, Tabs } from "../../ui/sidebar-tabs";
 import { motion } from "framer-motion";
-import useApplicationStore from "../../state/application-state";
-import { useShallow } from "zustand/react/shallow";
 import { cn } from "../../../lib/utils";
 
 export default function Sidebar() {
-  const { isInstalling } = useApplicationStore(
-    useShallow((s) => ({
-      isInstalling: s.installationContext.isInstalling,
-    }))
-  );
-
   const items: Tab[] = [
     {
       icon: <Home size={20} />,
@@ -26,15 +18,7 @@ export default function Sidebar() {
   ];
 
   return (
-    <motion.div
-      initial={{ x: 0 }}
-      animate={{
-        x: isInstalling ? -100 : 0,
-        width: isInstalling ? 0 : "3rem",
-        transition: { duration: 0.3 },
-      }}
-      className={cn("px-1 mt-4 h-full inline")}
-    >
+    <motion.div className={cn("w-12 px-1 mt-4 h-full inline")}>
       <Tabs tabs={items} />
     </motion.div>
   );
