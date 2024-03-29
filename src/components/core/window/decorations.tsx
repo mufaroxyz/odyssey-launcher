bb3d18308bd2cc83a3ffb55b90b921783a4ed1cc
Mufaro <mufaro.priv@gmail.com>
2024-03-29 18:26:09 +0100

    style: Center the icon

--- src/components/core/window/decorations.tsx
+++ src/components/core/window/decorations.tsx
@@ -9,7 +9,7 @@ export default function WindowDecorations() {
       data-tauri-drag-region
       className="h-[30px] bg-sidebar-bg-color select-none justify-between flex items-center top-0 right-0 left-0"
     >
-      <div className="w-12 flex justify-center">
+      <div className="w-12 mt-1 flex justify-center">
         <img src="/icons/icon.png" className="w-6 h-6 mt-2" />
       </div>
       <p data-tauri-drag-region className="text-sm text-white font-semibold">
